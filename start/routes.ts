24baff755b29d5e1077f539f3febf3f94a7ceb23
krunal dodiya <kunal.dodiya1@gmail.com>
2021-09-15 11:27:33 +0530

    added Dockerfile

--- start/routes.ts
+++ start/routes.ts
@@ -1,6 +1,11 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/', 'TodosController.show')
+Route.get('/testing', () => {
+  return {
+    name: 'krunal',
+  }
+})
 Route.get('/todos', 'TodosController.getAll')
 Route.post('/todos/create', 'TodosController.create')
 Route.post('/todos/update/:id', 'TodosController.update')
